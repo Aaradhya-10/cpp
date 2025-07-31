0abf19ca17a5cf54f02461f5d7413f654053e3df
Aaradhya <aaradhyadixit@gmail.com>
2025-07-31 22:37:02 +0530

    initializing

---
+++ temp.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <queue>
+#include <stack>
+#include <ctime>
+#include <cassert>
+#include <cassert>
+#include <complex>
+#include <string>
+#include <cstring>
+#include <chrono>
+#include <random>
+#include <bitset>
+#include <array>
+#include <climits>
+#include <deque>
+#include <numeric>
+#include <limits>
+#include <list>
+#include <fstream>
+#define lld long long int
+#define ulld unsigned long long
+#define vll vector<lld>
+#define pll pair<lld,lld>
+#define mll map<lld,lld>
+#define sll set<lld>
+#define pb push_back
+#define ss second
+#define ff first
+#define mp make_pair
+#define MOD 1000000007 //998244353
+#define answer(x) cout<<endl<<"answer : "<<x<<endl<<endl
+#define out(x) cout<<x<<endl
+#define MAXN 500005
+#define INF 5000000000000000000
+#define messi ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+using namespace std;
+int main() {messi lld t=1;//cin>>t;
+    while(t--){
+        lld n=0,m=0,k=0,i=0,j=0,p=0,q=0,x=0,y=0,z=0,ans=0,cnt=0,l=0,r=0,mid=0,lo=0,hi=0,lm=0,cur=0,curr=0;string s;bool flag=false;
+        cin>>n; //lld a[n]; for(i=0;i<n;++i) cin>>a[i];
+        //
+    }
+	return 0;
+}
+/*  CODED BY:-
+ ________________________________________
+|            __        __                |
+|  /\   /\  |__)  /\  |  \ |__| \ /  /\  |
+| /~~\ /~~\ |  \ /~~\ |__/ |  |  |  /~~\ |
+|________________________________________|
+
+*/
